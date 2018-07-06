bf52902dc23f639ec802858a126c58cc96c186e2
Ross Young <ross.young12@gmail.com>
2018-07-05 20:45:16 -0500

    initial commit

---
+++ todo.js
@@ -0,0 +1,26 @@
+var myList = document.querySelector("#tasks");
+
+//add tasks to a list via button click
+    
+var taskInput = document.querySelector("form input");
+var inputButton = document.querySelector("button");
+inputButton.onclick = function() {
+    if (taskInput.value != "") {
+        // take input value (value)
+        var newTaskValue = taskInput.value;
+
+        // create a new element (li)
+        var newTask = document.createElement("li");
+        newTask.innerHTML = newTaskValue;
+        
+        // append that to myList
+        myList.appendChild(newTask);
+    } else {
+        console.log ("empty");
+    }
+};
+    
+  //clear the input when the button's clicked
+
+//When a task is clicked, remove/strikethrough from the list
+  //alt, make a new 'completed' tasks list
